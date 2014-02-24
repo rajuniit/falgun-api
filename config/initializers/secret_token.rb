2ad5bb8d69c0206dedaa6c3ac19219f95253a96b
Raju Mazumder <rajuniit@gmail.com>
2014-02-24 14:42:40 +0600

    initial source

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FalgunApi::Application.config.secret_key_base = '769f66f96cd49cd3c4112ccee66abb5f9e94073f35b94e5dc438b83b7c57840554f39d0aff1d31869291b6c02b4e7829e78a0a76b6bb7f0015b1937e0fbdf15b'
